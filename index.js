f73c1c18e6e9a3ded8ef58dd358becb62a1bec6d
Ieti Lolo <31405843+jedidiahlolo@users.noreply.github.com>
2022-05-25 03:24:19 +1200

    Add Navigation

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const methodoverride = require("method-override");
 const mongoose = require("mongoose");
+const PORT = process.env.PORT
 
 // Config
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.listen(PORT, () => {
+  console.log("Listening on port: " + PORT);
+});
 
 // Routes
 app.get("/", (req, res) => {
