2e5dd44ec8e2fe504cce17cc95540779132f173a
Ieti Lolo <31405843+jedidiahlolo@users.noreply.github.com>
2022-05-24 20:38:06 +1200

    Add views and setup home route path

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const methodoverride = require("method-override");
+const mongoose = require("mongoose");
+
+// Config
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("home")
+});
