56da0e1c004758c29d3733f2943c3250154bd2f2
Ieti Lolo <31405843+jedidiahlolo@users.noreply.github.com>
2022-05-24 20:41:34 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const methodoverride = require("method-override");
 const mongoose = require("mongoose");
@@ -9,5 +10,5 @@ app.set("view engine", "ejs");
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("home")
+  res.render("home");
 });
